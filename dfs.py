cc6fde7cd23c7cc964120661f9260c21fb71a62b
elvinup <elvinauthuppan@gmail.com>
2018-08-22 10:58:53 -0400

    Added first bfs and dfs concepts

---
+++ dfs.py
@@ -0,0 +1,9 @@
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    for next in graph[start] - visited:
+        dfs(graph, next, visited)
+    return visited
+
+dfs(graph, 'C') # {'E', 'D', 'F', 'A', 'C', 'B'}
